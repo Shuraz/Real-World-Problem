d926b9266d7090b9b12751ecfc7bc289020a3fc5
Suraj Pokhrel <mrsurajpokhrel@gmail.com>
2021-11-25 15:31:23 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import CSSModule from "./App.module.css";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { FcBusinessman } from "react-icons/fc";
+import { MdOutlineMarkEmailUnread } from "react-icons/md";
+const url = "https://jsonplaceholder.typicode.com/users";
 
 function App() {
+  //used state to handle the API data
+  const [userState, setUserState] = useState([]);
+
+  //function to fetch api data
+  const getUser = () => {
+    axios.get(url).then((resp) => {
+      setUserState(resp.data);
+    });
+  };
+
+  // I have implemented react hooks for to optimize and avoid rendering of react virtual DOM .
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <h1>Contacts</h1>
+      <h5>Real World Problem</h5>
+      <div className={CSSModule.contact}>
+        {/* used condition to stop empty arry and then rendering with array map */}
+        {userState &&
+          userState.map((item) => {
+            return (
+              <div key={item.id} className={CSSModule.userbox}>
+                <h4 className={CSSModule.username}>{item.name} </h4>
+                <address>
+                  {item.address.suite} {item.address.street},{" "}
+                  {item.address.city}, {item.address.zipcode}
+                </address>
+                <a href="#">
+                  <MdOutlineMarkEmailUnread size={28} /> {item.email}
+                </a>
+                <a href="#">
+                  <FcBusinessman size={28} /> {item.website}
+                </a>
+              </div>
+            );
+          })}
+      </div>
+    </section>
   );
 }
 
